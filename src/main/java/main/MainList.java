6acf82147ef4e24d1a32211820d8139f097c90d9
Hicaro Brasil <Hicaro Brasil>
2023-03-28 04:46:30 -0300

    :construction: build: add a função delete e list nos mains

---
+++ src/main/java/main/MainList.java
@@ -0,0 +1,18 @@
+package main;
+
+import entities.Funcionario;
+
+import java.util.List;
+
+public class MainList {
+    public static void main(String[] args) {
+        MainUpdate.entityManager.getTransaction().begin();
+        List<Funcionario> funcionarioList = listar();
+        for(Funcionario f : funcionarioList)
+            System.out.println(f.getNome());
+    }
+    public static List<Funcionario> listar() {
+        String jpql = "FROM Funcionario f";
+        return MainUpdate.entityManager.createQuery(jpql, Funcionario.class).getResultList();
+    }
+}
