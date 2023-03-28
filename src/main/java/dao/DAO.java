de8ba563e962d089d9337dc926d7a80604c78bb2
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-03-28 02:39:37 -0300

    refactor main

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -8,7 +8,7 @@ public abstract class DAO {
     static EntityManagerFactory emf;
 
     static {
-        emf = Persistence.createEntityManagerFactory("dac");
+        emf = Persistence.createEntityManagerFactory("jpa");
     }
 
     protected EntityManager getEntityManager() {
